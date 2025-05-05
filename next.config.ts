1de1d8f09c1d2d2f1cb8cbf6f84275e2142374ca
paddyzong <youkobak@gmail.com>
2025-05-05 21:52:47 +1000

    fix tsconfig

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.0.228'],
   devIndicators: false,
-  "compilerOptions": {
-    "forceConsistentCasingInFileNames": true
-  },
 };
 
 export default nextConfig;
